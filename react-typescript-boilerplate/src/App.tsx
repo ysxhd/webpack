8e36453929e011ed62029143247afb13647d3073
zhangning <1109342967@qq.com>
2020-08-19 18:26:15 +0800

    add

--- react-typescript-boilerplate/src/App.tsx
+++ react-typescript-boilerplate/src/App.tsx
@@ -1,141 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 import {
-    List,
-    Avatar,
     Typography,
     Form,
-    Input,
-    Select,
-    DatePicker,
-    Menu,
-    Dropdown,
     Tabs
 } from "antd";
-import { DownOutlined } from "@ant-design/icons";
 import './App.scss';
-
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+import { todoListData } from "./utils/data";
 const { Title } = Typography;
-const { Option } = Select;
 const { TabPane } = Tabs;
 
-const todoListData = [
-    {
-        content: "图雀社区：汇聚精彩的免费实战教程",
-        user: "mRcfps",
-        time: "2020年3月2日 19:34",
-        isCompleted: false
-    },
-    {
-        content: "图雀社区：汇聚精彩的免费实战教程",
-        user: "pftom",
-        time: "2020年3月2日 19:34",
-        isCompleted: false
-    },
-    {
-        content: "图雀社区：汇聚精彩的免费实战教程",
-        user: "Holy",
-        time: "2020年3月2日 19:34",
-        isCompleted: false
-    },
-    {
-        content: "图雀社区：汇聚精彩的免费实战教程",
-        user: "crxk",
-        time: "2020年3月2日 19:34",
-        isCompleted: false
-    },
-    {
-        content: "图雀社区：汇聚精彩的免费实战教程",
-        user: "Pony",
-        time: "2020年3月2日 19:34",
-        isCompleted: false
-    }
-];
-
-const userList = [
-    {
-        id: "666666666",
-        name: "图雀社区",
-        avatar: "https://avatars0.githubusercontent.com/u/39240800?s=60&v=4"
-    },
-    {
-        id: "23410977",
-        name: "mRcfps",
-        avatar: "https://avatars0.githubusercontent.com/u/23410977?s=96&v=4"
-    },
-    {
-        id: "25455350",
-        name: "crxk",
-        avatar: "https://avatars1.githubusercontent.com/u/25455350?s=96&v=4"
-    },
-    {
-        id: "23410977",
-        name: "pftom",
-        avatar: "https://avatars0.githubusercontent.com/u/23410977?s=96&v=4"
-    },
-    {
-        id: "58352313",
-        name: "holy",
-        avatar: "https://avatars0.githubusercontent.com/u/58352313?s=96&v=4"
-    }
-];
-
-const menu = (
-    <Menu>
-        <Menu.Item>完成</Menu.Item>
-        <Menu.Item>删除</Menu.Item>
-    </Menu>
-);
-
-const TodoInput = () => {
-    return (
-        <div className="todoInput">
-            <Input type="text" placeholder="输入待办事项内容" />
-            <Select style={{ width: 80 }} size="small" defaultValue="666666666">
-                {userList.map((user, i) => (
-                    <Option key={i} value={user.id}>{user.name}</Option>
-                ))}
-            </Select>
-            <DatePicker
-                size="small"
-                style={{ marginLeft: "16px", marginRight: "16px" }}
-            />
-        </div>
-    );
-};
-
-function TodoList() {
-    return (
-        <List
-            className="demo-loadmore-list"
-            itemLayout="horizontal"
-            dataSource={todoListData}
-            renderItem={item => (
-                <List.Item
-                    actions={[
-                        <Dropdown overlay={menu}>
-                            <a key="list-loadmore-more">
-                                操作 <DownOutlined />
-                            </a>
-                        </Dropdown>
-                    ]}
-                >
-                    <List.Item.Meta
-                        avatar={
-                            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                        }
-                        title={<a href="https://ant.design">{item.user}</a>}
-                        description={item.time}
-                    />
-                    <div>{item.content}</div>
-                </List.Item>
-            )}
-        />
-    );
-}
 
 function App() {
     const callback = () => { };
+    const [todoList, setTodoList] = useState(todoListData);
+
+    const activeTodoList = todoList.filter(todo => !todo.isCompleted);
+    const completedTodoList = todoList.filter(todo => todo.isCompleted);
 
     const onFinish = () => { };
     const ref = useRef(null);
@@ -154,13 +37,13 @@ function App() {
             <div className="container">
                 <Tabs onChange={callback} type="card">
                     <TabPane tab="所有" key="1">
-                        <TodoList />
+                        <TodoList todoList={todoList}/>
                     </TabPane>
                     <TabPane tab="进行中" key="2">
-                        <TodoList />
+                        <TodoList todoList={activeTodoList}/>
                     </TabPane>
                     <TabPane tab="已完成" key="3">
-                        <TodoList />
+                        <TodoList todoList={completedTodoList}/>
                     </TabPane>
                 </Tabs>
             </div>
---
+++ react-typescript-boilerplate/src/utils/data.ts
@@ -0,0 +1,84 @@
+export interface Todo {
+    id: string;
+    user: string;
+    date: string;
+    content: string;
+    isCompleted: boolean;
+}
+  
+export interface User {
+    id: string;
+    name: string;
+    avatar: string;
+}
+
+export function getUserById(userId: string) {
+    return userList.filter(user => user.id === userId)[0];
+}
+  
+export const todoListData: Todo[] = [
+    {
+      id: "1",
+      content: "图雀社区：汇聚精彩的免费实战教程",
+      user: "23410977",
+      date: "2020年3月2日 19:34",
+      isCompleted: false
+    },
+    {
+      id: "2",
+      content: "图雀社区：汇聚精彩的免费实战教程",
+      user: "23410976",
+      date: "2020年3月2日 19:34",
+      isCompleted: false
+    },
+    {
+      id: "3",
+      content: "图雀社区：汇聚精彩的免费实战教程",
+      user: "58352313",
+      date: "2020年3月2日 19:34",
+      isCompleted: false
+    },
+    {
+      id: "4",
+      content: "图雀社区：汇聚精彩的免费实战教程",
+      user: "25455350",
+      date: "2020年3月2日 19:34",
+      isCompleted: false
+    },
+    {
+      id: "5",
+      content: "图雀社区：汇聚精彩的免费实战教程",
+      user: "12345678",
+      date: "2020年3月2日 19:34",
+      isCompleted: true
+    }
+  ];
+  
+  export const userList: User[] = [
+    {
+      id: "666666666",
+      name: "图雀社区",
+      avatar: "https://avatars0.githubusercontent.com/u/39240800?s=60&v=4"
+    },
+    {
+      id: "23410977",
+      name: "mRcfps",
+      avatar: "https://avatars0.githubusercontent.com/u/23410977?s=96&v=4"
+    },
+    {
+      id: "25455350",
+      name: "crxk",
+      avatar: "https://avatars1.githubusercontent.com/u/25455350?s=96&v=4"
+    },
+    {
+      id: "23410976",
+      name: "pftom",
+      avatar: "https://avatars0.githubusercontent.com/u/23410977?s=96&v=4"
+    },
+    {
+      id: "58352313",
+      name: "holy",
+      avatar: "https://avatars0.githubusercontent.com/u/58352313?s=96&v=4"
+    }
+  ];
+  
